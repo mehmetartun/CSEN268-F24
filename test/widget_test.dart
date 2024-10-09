359d1e4e54bc51ffb893b400cc53e17b168935ad
Mehmet Artun <mehmet@artun.com>
2024-10-08 17:22:12 -0700

    Lect 5 Starting Point

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lect4/main.dart';
+import 'package:CSEN268_F24/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
