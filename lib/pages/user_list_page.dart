eb2464f80a46ac6c994d88c46e86f046e432c872
Mehmet Artun <mehmet@artun.com>
2024-10-10 20:13:54 -0700

    Lecture 6 Finished

--- lib/pages/user_list_page.dart
+++ lib/pages/user_list_page.dart
@@ -1,45 +1,89 @@
+import 'package:CSEN268_F24/widgets/user_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lorem/flutter_lorem.dart';
 import 'package:uuid/v4.dart';
 import 'dart:math' as math;
 
 import '../model/user.dart';
+import '../widgets/user_card.dart';
 
 class UserListPage extends StatelessWidget {
   const UserListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const List<String> colors = [
-      "green",
-      "red",
-      "orange",
-      "yellow",
-      "blue",
-      "pink",
-      "cyan",
-      "magenta",
-      "coral",
-      "brown"
-    ];
-    List<User> userList = List.generate(20, (index) {
-      return User(
-          firstName: lorem(paragraphs: 1, words: 1),
-          lastName: lorem(paragraphs: 1, words: 1),
-          email:
-              "${lorem(paragraphs: 1, words: 1)}@${lorem(paragraphs: 1, words: 1)}.com",
-          uid: const UuidV4().generate(),
-          imageUrl:
-              'https://placehold.co/600x400/${colors[math.Random().nextInt(10)]}/${colors[math.Random().nextInt(10)]}.png');
-    });
-
-    // userList = List.generate(20, (index) {
-    //   return User.createMockUser();
+    // const List<String> colors = [
+    //   "green",
+    //   "red",
+    //   "orange",
+    //   "yellow",
+    //   "blue",
+    //   "pink",
+    //   "cyan",
+    //   "magenta",
+    //   "coral",
+    //   "brown"
+    // ];
+    // List<User> userList = List.generate(20, (index) {
+    //   return User(
+    //       firstName: lorem(paragraphs: 1, words: 1),
+    //       lastName: lorem(paragraphs: 1, words: 1),
+    //       email:
+    //           "${lorem(paragraphs: 1, words: 1)}@${lorem(paragraphs: 1, words: 1)}.com",
+    //       uid: const UuidV4().generate(),
+    //       imageUrl:
+    //           'https://placehold.co/600x400/${colors[math.Random().nextInt(10)]}/${colors[math.Random().nextInt(10)]}.png');
     // });
+
+    List<User> userList = List.generate(20, (index) {
+      return User.createMockUser();
+    });
 
     print(userList);
     return Scaffold(
-      body: Text("Hello World"),
+      //body: UserCard(user: userList[0]),
+      // body: UserListTile(user: userList[0]),
+      // body: ListView(
+      //     children: userList.map((user) {
+      //   return UserListTile(user: user);
+      // }).toList()),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: index == 0 ? 20 : 0),
+                    child: UserCard(user: userList[index]),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+                itemCount: userList.length,
+              ),
+            ),
+            Container(
+              height: 700,
+              child: ListView.separated(
+                // scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return UserListTile(user: userList[index]);
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+                itemCount: userList.length,
+              ),
+            ),
+            Container(height: 300, color: Colors.red, width: double.infinity),
+          ],
+        ),
+      ),
+      //Text("Hello World"),
       // body: Column(
       //   children: [
       //     Container(
