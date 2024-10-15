c47f31ccac7f381bd60b688e1baacb05406e056a
Mehmet Artun <mehmet@artun.com>
2024-10-15 15:38:36 -0700

    Lect7Start

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:CSEN268_F24/pages/user_list_page.dart';
+import 'package:CSEN268_F24/repositories/authentication/authentication_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'pages/my_home_page.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return RepositoryProvider(
+      create: (context) {
+        return (FirebaseAuthenticationRepository() as AuthenticationRepository);
+      },
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const LoginPage(),
       ),
-      home: const UserListPage(),
     );
   }
 }
