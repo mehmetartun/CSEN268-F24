b426cb40217483394c49c7aab61e1334f6bb8177
Mehmet Artun <mehmet@artun.com>
2024-10-16 16:41:33 -0700

    L8 start - go router basic implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:CSEN268_F24/navigation/router.dart';
 import 'package:CSEN268_F24/pages/home_page.dart';
 import 'package:CSEN268_F24/pages/sign_in/sign_in_page.dart';
 import 'package:CSEN268_F24/pages/stateful_login_page.dart';
@@ -5,7 +6,7 @@ import 'package:CSEN268_F24/repositories/authentication/authentication_repositor
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'navigation/navigator_routes.dart';
+import 'navigation/navigator_route.dart';
 import 'pages/login/login_page.dart';
 
 void main() {
@@ -21,19 +22,27 @@ class MyApp extends StatelessWidget {
       create: (context) {
         return (OktaAuthenticationRepository() as AuthenticationRepository);
       },
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          initialRoute: NavigatorRoute.home,
-          routes: {
-            NavigatorRoute.home: (context) => const HomePage(),
-            NavigatorRoute.noBloc: (context) => const StatefulLoginPage(),
-            NavigatorRoute.bloc: (context) => const SignInPage(),
-            NavigatorRoute.cubit: (context) => const LoginPage(),
-          }),
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routerConfig: router,
+      ),
+      // MaterialApp(
+      //     title: 'Flutter Demo',
+      //     theme: ThemeData(
+      //       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //       useMaterial3: true,
+      //     ),
+      //     initialRoute: NavigatorRoute.home,
+      //     routes: {
+      //       NavigatorRoute.home: (context) => const HomePage(),
+      //       NavigatorRoute.noBloc: (context) => const StatefulLoginPage(),
+      //       NavigatorRoute.bloc: (context) => const SignInPage(),
+      //       NavigatorRoute.cubit: (context) => const LoginPage(),
+      //     }),
     );
   }
 }
