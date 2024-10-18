7d75e783b7b17967e9c69a802e75a350c0ad350b
Mehmet Artun <mehmet@artun.com>
2024-10-17 17:35:11 -0700

    Nested Go Router

---
+++ lib/pages/router_demo_pages/router_demo_login.dart
@@ -0,0 +1,27 @@
+import 'package:CSEN268_F24/blocs/authentication/bloc/authentication_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RouterDemoLogin extends StatelessWidget {
+  const RouterDemoLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Login")),
+        body: Center(
+          child: Column(
+            children: [
+              Text("Router Demo Login"),
+              FilledButton(
+                child: Text("Login"),
+                onPressed: () {
+                  BlocProvider.of<AuthenticationBloc>(context)
+                      .add(AuthenticationLoginEvent());
+                },
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/pages/router_demo_pages/router_demo_profile.dart
@@ -0,0 +1,37 @@
+import 'package:CSEN268_F24/blocs/authentication/bloc/authentication_bloc.dart';
+import 'package:CSEN268_F24/navigation/routerdemo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+class RouterDemoProfile extends StatelessWidget {
+  const RouterDemoProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile")),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Router Demo Profile"),
+            FilledButton(
+              child: Text("Logout"),
+              onPressed: () {
+                BlocProvider.of<AuthenticationBloc>(context)
+                    .add(AuthenticationLogoutEvent());
+              },
+            ),
+            SizedBox(height: 20),
+            FilledButton(
+              child: Text("Show Detail"),
+              onPressed: () {
+                context.goNamed(RouteName.profileDetail);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
