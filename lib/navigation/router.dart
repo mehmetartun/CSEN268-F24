9f30046ee0316b52589252c0fd406c17b7aa8fc6
Mehmet Artun <mehmet@artun.com>
2024-10-22 12:17:02 -0700

    Start of Lecture 9

--- lib/navigation/router.dart
+++ lib/navigation/router.dart
@@ -1,41 +1,11 @@
-import 'package:CSEN268_F24/pages/home_page.dart';
-import 'package:CSEN268_F24/pages/login/login_page.dart';
-import 'package:CSEN268_F24/pages/sign_in/sign_in_page.dart';
-import 'package:CSEN268_F24/pages/stateful_login_page.dart';
 import 'package:go_router/go_router.dart';
 
-class RouteName {
-  static const home = "home";
-  static const bloc = "bloc";
-  static const cubit = "cubit";
-  static const noBloc = "noBloc";
-}
+import '../pages/generic_page.dart';
 
-final router = GoRouter(routes: [
+final GoRouter router = GoRouter(initialLocation: "/", routes: [
   GoRoute(
-      name: RouteName.home,
-      path: '/',
-      builder: (context, state) {
-        return const HomePage();
-      },
-      routes: [
-        GoRoute(
-            name: RouteName.bloc,
-            path: 'bloc',
-            builder: (context, state) {
-              return const SignInPage();
-            }),
-        GoRoute(
-            name: RouteName.cubit,
-            path: 'cubit',
-            builder: (context, state) {
-              return const LoginPage();
-            }),
-        GoRoute(
-            name: RouteName.noBloc,
-            path: 'noBloc',
-            builder: (context, state) {
-              return const StatefulLoginPage();
-            }),
-      ]),
+    path: "/",
+    name: "home",
+    builder: (context, state) => GenericPage(title: "Home"),
+  ),
 ]);
