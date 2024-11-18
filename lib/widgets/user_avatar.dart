06c515de82460017723869a2719617d548c1e2bb
Mehmet Artun <mehmet@artun.com>
2024-11-17 16:14:38 -0800

    test coverage|

--- lib/widgets/user_avatar.dart
+++ lib/widgets/user_avatar.dart
@@ -8,9 +8,20 @@ class UserAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String initials = "";
+    if (user.firstName.isNotEmpty) {
+      initials += user.firstName[0].toUpperCase();
+    }
+    if (user.lastName.isNotEmpty) {
+      initials += user.lastName[0].toUpperCase();
+    }
+    if (initials == "") {
+      initials = "-";
+    }
+
     return CircleAvatar(
       child: Text(
-        '${user.firstName[0].toUpperCase()}${user.lastName[0].toUpperCase()}',
+        initials,
       ),
     );
   }
